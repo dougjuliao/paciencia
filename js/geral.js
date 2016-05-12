ea99c1c4c33d0ef381663d82228406f3d8f012f5
Douglas Julião <douglas_juliao16@hotmail.com>
2016-05-12 06:17:09 -0300

    Arquivos iniciais - criando cartas ...

---
+++ js/geral.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	/*
+		Lógica do jogo:
+			Baralho completo 52 cartas,
+			Montes de cartas de 7,6,5,4,3,2,1
+			Cartas de compra    1,1,1,1,1,1,1,1
+	*/
+	var container = $('#container');
+
+
+})
