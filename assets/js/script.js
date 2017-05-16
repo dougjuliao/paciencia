fbfa3f53a2bd5ae0fba6b09bca4d08d6c4070251
Douglas Julião <douglas_juliao16@hotmail.com>
2017-05-16 14:47:06 -0300

    Add effect flip (test)

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
 		@GitHub: https://github.com/dougjuliao/
 		E-mail: douglas_juliao16@hotmail.com
 	*/
+	$('.card').each((i, ele) => { // flip effect
+		$(ele).on('click',function(){
+			$(this).toggleClass('turned');
+		});
+	});
 });
